931c4a82475674e2189e3ee77e143db27c225271
lyes <lyes.aici95@yahoo.fr>
2018-11-20 12:28:37 -0500

    🚀 Project scaffolding

---
+++ back/src/config/index.js
@@ -0,0 +1,9 @@
+export default {
+  db: {
+    host: process.env.DB_HOST || 'ds139243.mlab.com',
+    port: process.env.DB_PORT || '39243',
+    name: process.env.DB_NAME || 'glo-4035',
+    user: process.env.DB_USER || 'admin',
+    pwd: process.env.DB_PWD || 'team-9',
+  }
+}
